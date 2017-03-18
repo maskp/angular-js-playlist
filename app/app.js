6694e3842993588acc9ec3994c37b604428137b4
maskp <maskpuneet@gmail.com>
2017-03-18 00:12:16 -0400

    modules

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+myNinjaApp.config(function(){
+  //would fire b4 app runs
+});
+
+myNinjaApp.run(function(){
+  //would fire as application runs
+});
+
+myNinjaApp.controller(function(){
+
+});
